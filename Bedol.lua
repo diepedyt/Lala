2af2f586d2e5167b4357227505b75b1f8da77007
diepedyt <106111219+diepedyt@users.noreply.github.com>
2023-11-07 13:57:03 +0000

    Update Bedol.lua

--- Bedol.lua
+++ Bedol.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/-beta-/main/AutoParry.lua"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/diepedyt/freeScriptLoader/main/bunchOfScriptsLoader.lua'))()
